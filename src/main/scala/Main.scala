321ee89b2a6205688d684dae5eb87148d2b87531
daniil <daniilprohorov@gmail.com>
2018-05-24 15:19:15 +0300

    Добавил добавление из файла

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,10 +5,47 @@ object Main extends App {
   /** Наверное это все должно хранится в хеш таблице */
   import scala.collection.mutable.HashMap
   import scala.collection.Set
-  val City : HashMap[String, List[Double]] = HashMap(
-    ("Moscow", List(55.753960, 37.620393)), 
-    ("Rostov-on-Don", List(47.2313500, 39.7232800)),
-    ("Sankt-Petersburg", List(59.9386300, 30.3141300)))
+  import scala.io.Source
+
+  val city_filename = "city_coordinates.txt"
+
+  val relations_filename = "relations.txt"
+  def fileToHashMapD(filename : String) : HashMap[String, List[Double]] = {
+    var hashMapOf = new HashMap[String, List[Double]]
+    for(line <- Source.fromFile(filename).getLines){
+      val split1 = line.split(" -> ").toList
+      println(split1 mkString " ")
+      val split2 = split1(1).split(" ").toList.map(_.toDouble)
+      hashMapOf(split1(0)) = split2
+    }
+    print(hashMapOf)
+    hashMapOf
+    /* val listCity = Source.fromFile(filename).getLines.toList
+     val split1 = listCity.map(_.split(" -> "))
+     val split2 = split1
+       .map(x => x(1).split(" ").asInstanceOf[A])*/
+   }
+
+  val City = fileToHashMapD(city_filename) 
+  /*val City : HashMap[String, List[Double]] = HashMap(
+    //("Moscow" -> List(55.753960, 37.620393)), 
+    "Rostov-on-Don" -> List(47.2313500, 39.7232800),
+    //("Sankt-Petersburg" -> List(59.9386300, 30.3141300))
+    "Chaltur" -> List(47.2847700, 39.4823200),
+    "Azov" -> List(47.1077900, 39.4164800),
+    "Aksai" -> List(47.2583800, 39.8667500),
+    "Bataisk" -> List(47.1397500, 39.7518100),
+    "Novocherkassk" -> List(47.4201800, 40.0913200),
+    "Zernograd" -> List(46.8451800, 40.3083400),
+    "Taganrog" -> List(47.2361700, 38.8968800),
+    "Novoshahtinsk" -> List(47.7603700, 39.9333500),
+    "Shahtu" -> List(47.7091100, 40.2144300), 
+    "Semikarakorsk" -> List(47.5194300, 40.8112000),
+    "Volgodonsk" -> List(47.5136100, 42.1513900)
+    )
+  val Relation = HashMap(
+    
+    )*/
 
   /** TODO Написать функцию, создающую HashMap из файла */
   
@@ -29,9 +66,7 @@ object Main extends App {
     /** Основная формула для вычисления расстояния */
     acos( sin(Ar(0))*sin(Br(0)) + cos(Ar(0))*cos(Br(0))*cos(Ar(1) - Br(1)) ) * R 
   }
-  /** Форматируем результат командой 
-    * "% <число символов до запятой> . < число символов после запятой>" format(x) */
-  println("%.3f" format(getLen(City("Moscow"), City("Rostov-on-Don"))))
+
   /** Библиотеки Graph  */
   import scalax.collection.Graph
   import scalax.collection.GraphPredef._
@@ -86,9 +121,8 @@ object Main extends App {
       val temp_edge = n(edge._1) pathTo n(edge._2)
       /** Получаем вес этого ребра */
       val w_edge = temp_edge get 
-      val weight = w_edge.weight
+      val weight = w_edge.weight.toInt
       count += 1
-      val str = 
       print(f"$count. ${edge._1}%-20s ~ ${edge._2}%-20s Weight = $weight \n")
     }
   }
