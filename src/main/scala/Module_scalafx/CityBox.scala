567e74d73c982526bf1aa627e971dbc1ed467483
Михаил <39408694+Garenta@users.noreply.github.com>
2018-06-03 23:56:29 +0300

    KK

--- src/main/scala/Module_scalafx/CityBox.scala
+++ src/main/scala/Module_scalafx/CityBox.scala
@@ -8,26 +8,18 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.paint.{LinearGradient, Stops}
 import scalafx.scene.text.Text
 import scalafx.geometry.Pos
-import Database.DatabaseApp
-
 /** Получаем словарь (map) */
 
 
-class City {
+class CityBox {
 
-  val Temp_DataBase = new DatabaseApp
-  val DataBase = Temp_DataBase.DataBase
-
-  val city = DataBase.city()
-  val relation = DataBase.relation()
-  val names = DataBase.names()
-
+  def getBoxes (name: String) =
   new VBox {
     padding = Insets(20)
     alignment = Pos.Center
     children = Seq(
       new Text {
-        text = ""
+        text = name
         style = "-fx-font-size: 12pt"
         fill = new LinearGradient(
           endX = 0,
--- src/main/scala/Module_scalafx/Scene_App.scala
+++ src/main/scala/Module_scalafx/Scene_App.scala
@@ -1,5 +1,8 @@
 package Module_scalafx
 
+
+
+import Database.DatabaseApp
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
@@ -7,15 +10,23 @@ import scalafx.scene.Scene
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.Color._
 
+object Scene_App extends JFXApp{
 
-object Interface extends JFXApp{
+  val Temp_DataBase = new DatabaseApp
+  val DataBase = Temp_DataBase.DataBase
+
+  val сity = DataBase.city()
+  val relation = DataBase.relation()
+  val names = DataBase.names()
+  val CityBoxApp = new CityBox
+
   stage = new PrimaryStage {
     title = "ScalaFX Hello World"
     scene = new Scene {
       fill = Black
       content = new HBox {
         padding = Insets(20)
-        children = Seq()
+        children = names.values.map(CityBoxApp.getBoxes(_)).toSeq
       }
     }
   }
