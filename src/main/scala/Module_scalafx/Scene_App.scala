a583dee3d88bcb0c899412b942d61ae91aef8e0f
daniil <daniilprohorov@gmail.com>
2018-07-07 11:55:59 +0300

    Научились рисовать линииgit add Scene_App.scala

--- src/main/scala/Module_scalafx/Scene_App.scala
+++ src/main/scala/Module_scalafx/Scene_App.scala
@@ -9,6 +9,9 @@ import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.Pane
 import scalafx.scene.paint.Color._
+import scalafx.scene.shape.Line
+import scalafx.scene.paint.Color
+
 
 object Scene_App extends JFXApp{
 
@@ -28,6 +31,7 @@ object Scene_App extends JFXApp{
       startCity : Tuple2[Double, Double] = startCity) : Tuple2[Double, Double] = {
     Tuple2(((city(id)(1) - startCity._1) * dispersion + 500).toDouble, ( -1 * (city(id)(0) - startCity._2) * dispersion * 4 + 400).toDouble)
   }
+
     stage = new PrimaryStage {
     width = 800
     height = 800
@@ -35,10 +39,20 @@ object Scene_App extends JFXApp{
     title = "ScalaFX Hello World"
     scene = new Scene {
       fill = Black
-      content = new Pane {
-        padding = Insets(80)
-        children = city.keySet.map(x => CityBoxApp.getBoxes(names(x), getXY(x)._1, getXY(x)._2)).toSeq
+      val road = new Line {
+          startX = 10
+          startY = 10
+          endX = 500
+          endY = 500
+          fill = Color.Red
+          stroke = Color.Blue
+          strokeWidth = 10d }
+        val cities = city.keySet.map(x => CityBoxApp.getBoxes(names(x), getXY(x)._1, getXY(x)._2)).toList
+
+        content = new Pane {
+          padding = Insets(80)
+          children = road :: cities  
+        } 
       }
     }
-  }
 }
